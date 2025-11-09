b1e3934fc45eab8620a6c98a57b5df03f86016e1
Assefa Endale <Abassiendale96@gmail.com>
2025-11-09 13:26:00 +0300

    adding files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from .models import Book, Library
 
-# Create your views here.
+# ---------------------------
+# 1️⃣ Function-Based View
+# ---------------------------
+
+def list_books(request):
+    """Display a list of all books with their authors."""
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
+
+
+# ---------------------------
+# 2️⃣ Class-Based View
+# ---------------------------
+
+class LibraryDetailView(View):
+    """Display details of a specific library and its books."""
+    template_name = 'library_detail.html'
+
+    def get(self, request, pk):
+        library = get_object_or_404(Library, pk=pk)
+        return render(request, self.template_name, {'library': library})
