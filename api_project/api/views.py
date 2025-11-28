1891ca62915fb81111e40a01259386205b32ac77
Assefa Endale <Abassiendale96@gmail.com>
2025-11-28 23:56:05 +0300

    adding files

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,20 @@
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
+
+# Existing list view (optional, can keep or remove)
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for all CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing, creating, updating, and deleting books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,4 +26,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing, creating, updating, and deleting books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+class Bookview(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
