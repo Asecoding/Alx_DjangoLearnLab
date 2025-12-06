c6473feee8d67ef6dd78507b15921a788279b518
Assefa Endale <Abassiendale96@gmail.com>
2025-12-06 20:07:22 +0300

    adding model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,21 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """
+    Post model represents each blog article.
+    - title: title of the blog post
+    - content: full blog text content
+    - published_date: automatically records date/time when created
+    - author: foreign key referencing Django's built-in User model
+    """
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
