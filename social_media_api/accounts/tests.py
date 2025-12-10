42e71965f45aafac91dce4ba3f9621db06f04aa5
Assefa Endale <Abassiendale96@gmail.com>
2025-12-10 13:12:31 +0300

    adding test files

--- social_media_api/accounts/tests.py
+++ social_media_api/accounts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class FollowTests(APITestCase):
+    def setUp(self):
+        self.a = User.objects.create_user(username='a', password='pass')
+        self.b = User.objects.create_user(username='b', password='pass')
+        self.client.login(username='a', password='pass')
+
+    def test_follow_unfollow(self):
+        url_follow = f'/api/accounts/follow/{self.b.id}/'
+        resp = self.client.post(url_follow)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(self.b in self.a.following.all())
+
+        url_unfollow = f'/api/accounts/unfollow/{self.b.id}/'
+        resp = self.client.post(url_unfollow)
+        self.assertEqual(resp.status_code, 200)
+        self.assertFalse(self.b in self.a.following.all())
+
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from .views import RegisterView, LoginView, ProfileView, FollowUserView, UnfollowUserView, FollowersListView, FollowingListView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('followers/<int:user_id>/', FollowersListView.as_view(), name='user-followers'),
+    path('followers/', FollowersListView.as_view(), name='my-followers'),
+    path('following/<int:user_id>/', FollowingListView.as_view(), name='user-following'),
+    path('following/', FollowingListView.as_view(), name='my-following'),
 ]
 
