69b5fb9700cef6ecf617242a9a2f97d937cb8338
Assefa Endale <Abassiendale96@gmail.com>
2025-12-10 11:40:41 +0300

    updating settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -55,9 +55,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         ),
-    'DEFAULT_PAGINATION_CLASS': ( 
-        'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10),
-    'DEFAULT_FILTER_BACKENDS': ( 'rest_framework.filters.SearchFilter', 'rest_framework.filters.OrderingFilter'),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS':  'rest_framework.filters.SearchFilter', 'rest_framework.filters.OrderingFilter'),
 }
 
 # Media settings for profile pictures
