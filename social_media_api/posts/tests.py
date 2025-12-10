ffc46f9aecf643031a2772e627a305987a4d84be
Assefa Endale <Abassiendale96@gmail.com>
2025-12-10 12:14:11 +0300

    adding test files

--- social_media_api/posts/tests.py
+++ social_media_api/posts/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
 
-# Create your tests here.
+User = get_user_model()
+
+class PostCommentAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='tester', password='pass1234')
+        self.other = User.objects.create_user(username='other', password='pass1234')
+        self.client.login(username='tester', password='pass1234')  # or use token auth
+
+    def test_create_post(self):
+        url = '/api/posts/'
+        data = {'title': 'Test', 'content': 'Body'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Post.objects.first().author, self.user)
+
+    def test_only_owner_can_delete_post(self):
+        post = Post.objects.create(author=self.other, title='X', content='Y')
+        url = f'/api/posts/{post.id}/'
+        # logged in as tester should not be able to delete other's post
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 403)
+
+    def test_create_comment(self):
+        post = Post.objects.create(author=self.other, title='X', content='Y')
+        url = '/api/comments/'
+        data = {'post': post.id, 'content': 'Good one'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(Comment.objects.first().author, self.user)
+
