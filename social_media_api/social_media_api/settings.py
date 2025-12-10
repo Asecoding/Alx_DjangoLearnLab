0e3dffa9bda5385567cbba05a3bfeed3257d21b3
Assefa Endale <Abassiendale96@gmail.com>
2025-12-10 11:20:07 +0300

    adding file

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -52,11 +52,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',  # optional
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+        ],
+    'DEFAULT_PAGINATION_CLASS': [ 
+        'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10],
     'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter', 'rest_framework.filters.OrderingFilter'],
 }
 
